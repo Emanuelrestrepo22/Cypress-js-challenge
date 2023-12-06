5d1c2f1f0ce7b4b63bbefd555ec1330d81df7d5a
Franco Bizzarri <105225095+franbizzarri@users.noreply.github.com>
2023-12-06 13:30:46 -0300

    TC2 created but not finished

--- cypress/e2e/Tests/marathon-challenge/MyElements.cy.js
+++ cypress/e2e/Tests/marathon-challenge/MyElements.cy.js
@@ -8,11 +8,11 @@ describe('Challenge Elements', () => {
 	// Preconditions
 	beforeEach('PRC: User must be located in the Text-Box page', () => {
 		cy.visit('/text-box');
-		cy.url().should('contain', 'text-box');
+		// cy.url().should('contain', 'text-box');
 	});
 
 	// Test Cases
-	it('TC1: Validate submit form with valid credentials', () => {
+	it('TEXTBOX - TC1: Validate submit form with valid credentials', () => {
 		// ARRANGE (preparación de variables reutilizables)
 
 		const fullname = faker.name.fullName();
@@ -32,5 +32,17 @@ describe('Challenge Elements', () => {
 		cy.get('p#email').should('contain', email);
 		cy.get('p#currentAddress').should('contain', currAdd);
 		cy.get('p#permanentAddress').should('contain', permAdd);
+	});
+
+	it.only('CHECKBOX - TC2: Validate check boxes and labels displayed afterwards', () => {
+		cy.visit('/checkbox');
+		cy.get('[for^=tree-node]').should('have.length', 1);
+		cy.get('[aria-label = "Expand all"]').click();
+		cy.get('[for^=tree-node]').should('have.length', 17);
+		cy.get('[type=checkbox]').eq(0).check({ force: true });
+		cy.get('[type=checkbox]').eq(0).should('be.checked');
+		cy.get('[type=checkbox]').eq(5).uncheck({ force: true });
+		cy.get('[type=checkbox]').eq(5).should('not.be.checked');
+		//CONTINUAR
 	});
 });
